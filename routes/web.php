0efb7db5f6b04699c5cfd14556da70a3d46c2a79
Mohammad Brilliant <brilliantmoch@gmail.com>
2024-12-19 17:43:10 +0700

    praktikum15

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\EmployeeController;
+
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware('auth')->group(function () {
+    Route::resource('employees', EmployeeController::class);
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware('auth')->group(function () {
+    Route::resource('employees', EmployeeController::class);
+    Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
+});
+
